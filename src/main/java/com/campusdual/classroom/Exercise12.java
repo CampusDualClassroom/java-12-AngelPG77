f24e08e4d1f1d5d0a807d6150fa0cf8a9565d083
AngelPG77 <angelpugagomez@gmail.com>
2025-05-23 13:18:32 +0200

    Ejercicio 12 Completado

--- src/main/java/com/campusdual/classroom/Exercise12.java
+++ src/main/java/com/campusdual/classroom/Exercise12.java
@@ -2,6 +2,47 @@ package com.campusdual.classroom;
 
 public class Exercise12 {
     public static void main(String[] args) {
+        Car coche1 = new Car();
+        Car coche2 = new Car("Citröen", "Xsara Picasso", "Diesel");
+
+        System.out.println("Esta el tacometro a cero: " + coche2.isTachometerGreaterThanZero());
+        coche2.start();
+        System.out.println("Esta el tacometro a cero: " + coche2.isTachometerGreaterThanZero());
+        coche2.start();
+        coche2.stop();
+        coche2.start();
+        coche2.speedometer = 5;
+        coche2.stop();
+        for (int i = 1; i <= 12; i++) {
+            coche2.accelerate();
+        }
+        coche2.brake();
+        for (int i = 1; i <= 12; i++) {
+            coche2.brake();
+        }
+        coche2.turnAngleOfWheels(20);
+        coche2.turnAngleOfWheels(20);
+        coche2.turnAngleOfWheels(20);
+        coche2.turnAngleOfWheels(-20);
+        coche2.turnAngleOfWheels(-20);
+        coche2.turnAngleOfWheels(-20);
+        coche2.turnAngleOfWheels(-20);
+        coche2.turnAngleOfWheels(-20);
+        coche2.stop();
+        coche2.setReverse(true);
+        coche2.setReverse(false);
+        coche2.start();
+        for (int i = 1; i <= 6; i++) {
+            coche2.accelerate();
+        }
+        coche2.setReverse(true);
+
+
+
+
+
+
+
 
     }
 
